fa134899e28653ad527a337c910ab60c9d0dd332
Nathan Heaps <1282393+nsheaps@users.noreply.github.com>
2026-01-21 15:28:46 -0500

    fix!: get rid of claude-tools

--- plugins/claude-tools/.release-it.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: "../../.release-it.base.json",
-  plugins: {
-    "@release-it/bumper": {
-      in: ".claude-plugin/plugin.json",
-      out: ".claude-plugin/plugin.json",
-    },
-  },
-};
